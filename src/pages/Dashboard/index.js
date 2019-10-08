3c1edd509222fb350fa01463dd72e4372bb9b515
VMoraes26 <vimoraescarvalho@gmail.com>
2019-10-08 20:38:58 -0300

    Criando rotas

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+export default function Dashboard() {
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user')
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            })
+            console.log(response)
+        }
+        loadSpots();
+    }, [])
+    return (
+        <>
+            <ul className="spot-list">
+            </ul>
+        </>
+    )
+};
